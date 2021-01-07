a86ae225c3dbbe159fed1b16d981f2a829d54ae8
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-01-07 18:46:52 -0300

    form ready

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
   def dashboard
+    @project = Project.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'form/new'
+  get 'form/create'
   get 'projects/dashboard'
+
+  root 'projects#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
